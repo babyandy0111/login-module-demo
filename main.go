68dec81d5396dd799b1fac511d9eb6304dad7e3d
Andy <babyandy0111@gmail.com>
2021-12-20 15:58:53 +0800

    完成line login

--- main.go
+++ main.go
@@ -5,12 +5,13 @@ import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/joho/godotenv"
 	"github.com/markbates/goth"
+	"github.com/markbates/goth/providers/apple"
 	"github.com/markbates/goth/providers/facebook"
 	"github.com/markbates/goth/providers/google"
+	"github.com/markbates/goth/providers/line"
 	"github.com/shareed2k/goth_fiber"
 	"log"
 	"os"
-	"strconv"
 )
 
 func main() {
@@ -20,19 +21,18 @@ func main() {
 		log.Fatal("Error loading .env file")
 	}
 
-	callBaclUrl := "https://auth.codegenapps.com"
+	callBaclUrl := "http://localhost:8080"
 	app := fiber.New()
 	googleConfig := google.New(os.Getenv("GOOGLE_CLIENT_ID"), os.Getenv("GOOGLE_CLIENT_SECRET"), callBaclUrl+"/auth/google/callback")
 	facebookConfig := facebook.New(os.Getenv("FACEBOOK_CLIENT_ID"), os.Getenv("FACEBOOK_CLIENT_SECRET"), callBaclUrl+"/auth/facebook/callback")
-	goth.UseProviders(googleConfig, facebookConfig)
+	appleConfig := apple.New(os.Getenv("APPLE_KEY"), os.Getenv("APPLE_SECRET"), callBaclUrl+"/auth/apple/callback", nil, apple.ScopeName, apple.ScopeEmail)
+	lineConfig := line.New(os.Getenv("LINE_KEY"), os.Getenv("LINE_SECRET"), callBaclUrl+"/auth/line/callback", "profile", "openid", "email")
+	goth.UseProviders(googleConfig, facebookConfig, lineConfig, appleConfig)
 
 	// google login 因為需要丟參數
 	// 因此需要轉止一次
 	app.Get("/auth/:uid/:provider/login", func(ctx *fiber.Ctx) error {
-		uid, err := Bin2hex(ctx.Params("uid"))
-		if err != nil {
-			return err
-		}
+		uid := ctx.Params("uid")
 		log.Println(uid)
 		newURL := fmt.Sprintf("/auth/%s?state=%s", ctx.Params("provider"), uid)
 		log.Println(newURL)
@@ -49,11 +49,8 @@ func main() {
 		return ctx.Redirect(url, fiber.StatusTemporaryRedirect)
 	})
 
-	app.Get("/:provider/callback", func(ctx *fiber.Ctx) error {
-		state, err := Hex2Bin(ctx.Query("state"))
-		if err != nil {
-			return err
-		}
+	app.Get("/auth/:provider/callback", func(ctx *fiber.Ctx) error {
+		state := ctx.Query("state")
 		user, err := goth_fiber.CompleteUserAuth(ctx)
 		if err != nil {
 			return err
@@ -67,7 +64,7 @@ func main() {
 		return ctx.Redirect(newURL, fiber.StatusTemporaryRedirect)
 	})
 
-	app.Get("/:provider/logout", func(ctx *fiber.Ctx) error {
+	app.Get("/auth/:provider/logout", func(ctx *fiber.Ctx) error {
 		if err := goth_fiber.Logout(ctx); err != nil {
 			return err
 		}
@@ -78,19 +75,4 @@ func main() {
 	if err := app.Listen("localhost:8080"); err != nil {
 		log.Fatal(err)
 	}
-}
-func Hex2Bin(hex string) (string, error) {
-	ui, err := strconv.ParseUint(hex, 16, 64)
-	if err != nil {
-		return "", err
-	}
-	return fmt.Sprintf("%016b", ui), nil
-}
-
-func Bin2hex(str string) (string, error) {
-	i, err := strconv.ParseInt(str, 2, 0)
-	if err != nil {
-		return "", err
-	}
-	return strconv.FormatInt(i, 16), nil
 }
